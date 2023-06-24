804417f91d1573d16f01c4ed920e84412e0af3a2
Dudous <sampaioeeduardo36@gmail.com>
2023-06-23 21:56:05 -0300

    sla

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController,  ToastController, } from '@ionic/angular';
+import { DogApiService } from 'src/services/dog-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  dados: any;
 
+  constructor(
+  //  public mensagem: ToastController,
+  //  public nav: NavController,
+  //  private dog: DogApiService,
+  ) {}
+
+//  RandomDog() {
+    
+  //   this.dog.RandomImg().subscribe(
+  //     (resp) => {
+  //       this.dog = resp;
+  //     }
+  //   )
+  // }
 }
